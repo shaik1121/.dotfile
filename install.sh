1c573cff43fbd408e7d27e0842f840da56728162
shaik1121 <sk.muzakkirhussain@gmail.com>
2022-07-16 22:08:09 +0530

    first glimps

---
+++ install.sh
@@ -0,0 +1,32 @@
+
+# install nix
+curl -L https://nixos.org/nix/install | sh
+
+# source nix
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+# install packages
+nix-env -iA \
+	nixpkgs.zsh \
+	nixpkgs.git \
+	nixpkgs.neovim \
+	nixpkgs.stow \
+	nixpkgs.yarn \
+	nixpkgs.fzf \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.gnumake \
+	nixpkgs.gcc \
+	nixpkgs.direnv
+
+# stow dotfiles
+stow git
+stow nvim
+stow tmux
+stow zsh
+
+# add zsh as a login shell
+command -v zsh | sudo tee -a /etc/shells
+
+# use zsh as default shell
+sudo chsh -s $(which zsh) $USER
